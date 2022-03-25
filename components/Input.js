72d5835a7baac03691ed48efb23304761b94feb7
Jaedon Lee <31083980+jaedonl@users.noreply.github.com>
2022-03-25 18:24:53 -0400

    -

--- components/Input.js
+++ components/Input.js
@@ -42,20 +42,15 @@ const Input = () => {
         setShowEmojis(false)
     }
 
-    // const addImageToPost = (e) => {
-    //     const reader = new FileReader()
-    //     if (e.target.files[0]) {
-    //         reader.readAsDataURL(e.target.files[0])
-    //     }
-                
-    //     reader.onload = (readerEvent) => {
-    //         setSelectedFile(readerEvent.target.result)
-    //     }
-    // }
-
     const addImageToPost = (e) => {
-        const [file] = e.target.files;        
-        setSelectedFile(URL.createObjectURL(file));
+        const reader = new FileReader()
+        if (e.target.files[0]) {
+            reader.readAsDataURL(e.target.files[0])
+        }
+                
+        reader.onload = (readerEvent) => {
+            setSelectedFile(readerEvent.target.result)
+        }
     }
 
     const addEmoji = (e) => {
@@ -66,10 +61,6 @@ const Input = () => {
         // setInput(input + e.native)
         setInput(input + e.native)
     }    
-
-    useEffect(() => {
-        console.log(selectedFile)
-    })
 
     return (
         <div className={`border-b border-[#EFF3F4]-700 p-3 flex space-x-3 overflow-y-scroll`}>
@@ -88,7 +79,7 @@ const Input = () => {
                     {selectedFile && (
                         <div className="relative">
                             <div
-                                onClick={() => setSelectedFile(null)} 
+                                onClick={() => setSelectedFile('')} 
                                 className="absolute w-8 h-8 bg-[#15181c] hover:bg-[#272c26] bg-opacity-75 rounded-full flex items-center justify-center top-1 right-1 cursor-pointer">
                                 <XIcon className="text-white h-5" />
                             </div>
@@ -97,46 +88,48 @@ const Input = () => {
                         </div>
                     )}                    
                 </div>
+                {!loading && (
+                    <div className="flex items-center justify-between pt-2.5">
+                        <div className="flex items-center">
+                            <div className="icon" onClick={() => filePickerRef.current.click()}>
+                                <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
+                                <input type="file" hidden ref={filePickerRef} onChange={addImageToPost} />
+                                
+                            </div>
+                            <div className="icon">
+                                <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />                            
+                            </div>                        
+                            <div className="icon">
+                                <CalendarIcon className="h-[22px] text-[#1d9bf0]" />                            
+                            </div>
+                            <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
+                                <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />                            
+                            </div>
 
-                <div className="flex items-center justify-between pt-2.5">
-                    <div className="flex items-center">
-                        <div className="icon" onClick={() => filePickerRef.current.click()}>
-                            <PhotographIcon className="h-[22px] text-[#1d9bf0]" />
-                            <input type="file" hidden ref={filePickerRef} onChange={addImageToPost} />
-                            
+                            {showEmojis && (
+                                <Picker
+                                    onSelect={addEmoji}
+                                    style={{
+                                        position: "absolute",
+                                        marginTop: "465px",                     
+                                        maxWidth: "320px",
+                                        borderRadius: "20px",
+                                    }}                                
+                                    theme="auto"                                                  
+                                />
+                            )}
                         </div>
-                        <div className="icon">
-                            <ChartBarIcon className="h-[22px] text-[#1d9bf0]" />                            
-                        </div>                        
-                        <div className="icon">
-                            <CalendarIcon className="h-[22px] text-[#1d9bf0]" />                            
-                        </div>
-                        <div className="icon" onClick={() => setShowEmojis(!showEmojis)}>
-                            <EmojiHappyIcon className="h-[22px] text-[#1d9bf0]" />                            
-                        </div>
-
-                        {showEmojis && (
-                            <Picker
-                                onSelect={addEmoji}
-                                style={{
-                                    position: "absolute",
-                                    marginTop: "465px",                     
-                                    maxWidth: "320px",
-                                    borderRadius: "20px",
-                                }}                                
-                                theme="auto"                                                  
-                            />
-                        )}
+                        <button 
+                            type="button" 
+                            disabled={!input.trim() && !selectedFile}
+                            className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"                        
+                            onClick={sendPost}
+                            >
+                            Tweet
+                        </button>                    
                     </div>
-                    <button 
-                        type="button" 
-                        disabled={!input.trim() && !selectedFile}
-                        className="bg-[#1d9bf0] text-white rounded-full px-4 py-1.5 font-bold shadow-md hover:bg-[#1a8cd8] disabled:hover:bg-[#1d9bf0] disabled:opacity-50 disabled:cursor-default"                        
-                        onClick={sendPost}
-                        >
-                        Tweet
-                    </button>                    
-                </div>
+                )}
+                
             </div>          
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios'
 import Head from 'next/head'
 import Sidebar from '../components/Sidebar'
 import Feed from '../components/Feed';
+import Login from '../components/Login';
+import { getProviders, getSession, useSession } from "next-auth/react"
+// getProviders() method returns the list of providers currently configured for sign in.
+// useSession() React Hook in the NextAuth.js client is the easiest way to check if someone is signed in.
+// getSession() method which can be called client or server side to return a session.
 
-export default function Home() {
+export default function Home({ trendingResults, followResults, providers }) {
+  const { data: session, status } = useSession()
+
+  if (!session) return <Login providers={providers} />
+
   return (
     <div className="">
       <Head>
@@ -19,4 +29,25 @@ export default function Home() {
       </main>
     </div>
   )
+}
+
+export const getServerSideProps = async (context) => {
+  const resTrending = await axios.get(`https://jsonkeeper.com/b/NKEV`)
+  const trendingResults = resTrending.data
+
+  const resFollowing = await axios.get("https://jsonkeeper.com/b/WWMJ")
+  const followResults = resFollowing.data
+
+  const providers = await getProviders()
+  const session = await getSession(context) 
+    
+
+  return { 
+    props: { 
+      trendingResults, 
+      followResults,
+      providers,
+      session 
+    } 
+  }
 }
